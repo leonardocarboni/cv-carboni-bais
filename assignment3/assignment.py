75ab0882f09300bd6554ae93662dbb331fb04e95
Leonardo Carboni <43882851+leonardocarboni@users.noreply.github.com>
2023-03-19 14:42:27 +0100

    ultima pulizia

--- assignment3/assignment.py
+++ assignment3/assignment.py
@@ -1,8 +1,10 @@
 import os
-import glm
 from time import time
+
+import glm
 from scipy.spatial import distance
 from sklearn.preprocessing import normalize
+
 from utils import *
 
 # flag to perform tracking
@@ -23,7 +25,7 @@ criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 labels_to_color = {0: (255, 0, 0), 1: (0, 255, 0),
                    2: (0, 0, 255), 3: (255, 0, 255)}
 
-# map to select the hand picked frame to build the color model for each camera
+# map to select the hand-picked frame to build the color model for each camera
 cam_to_frame = {0: 10, 1: 0, 2: 41, 3: 52}
 
 # loading parameters of the cameras and masks of all frames
@@ -54,7 +56,7 @@ def generate_grid(width, depth):
     for x in range(width):
         for z in range(depth):
             data.append([x * block_size - width / 2, -
-                         block_size, z * block_size - depth / 2])
+            block_size, z * block_size - depth / 2])
             colors.append([1.0, 1.0, 1.0] if (x + z) % 2 == 0 else [0, 0, 0])
     return data, colors
 
@@ -85,7 +87,7 @@ def reconstruct_voxels(n_frame):
             visible_voxels.append(
                 [x_voxels / 75, -z_voxels / 75, y_voxels / 75])
 
-    # extract x and y coordinates for clustering (ignore verticality)
+    # extract x and y coordinates for clustering (ignore vertical)
     voxels_to_cluster = np.array([[x[0], x[2]]
                                   for x in visible_voxels], dtype=np.float32)
     # clustering
@@ -96,10 +98,10 @@ def reconstruct_voxels(n_frame):
     voxels_to_remove = []
 
     # for each cluster
-    for label, center in enumerate(centers):
+    for label_center, center in enumerate(centers):
         # get voxels belonging to the cluster
         filtered_list = [lst for lst, current_lab in zip(
-            visible_voxels, labels) if current_lab == label]
+            visible_voxels, labels) if current_lab == label_center]
 
         avg_dist_from_center = 0
 
@@ -108,7 +110,7 @@ def reconstruct_voxels(n_frame):
             # get its euclidean distance from the center
             avg_dist_from_center += distance.euclidean((x, y), center)
         avg_dist_from_center = avg_dist_from_center / \
-            len(filtered_list)  # calculate average
+                               len(filtered_list)  # calculate average
 
         # for each voxel of the cluster
         for x, z, y in filtered_list:
@@ -135,14 +137,14 @@ def set_voxel_positions(width, height, depth, n_frame):
     # function to handle the online section:
     # for each frame, it gets the 3D reconstruction, selects the least occluded camera
     # and performs the matching task
-    # input: frame to reconstruct (width height and depth are not used as the lookup table is global, built on execution)
+    # input: frame to reconstruct (width, height, depth are not used as the lookup table is global, built on execution)
     # output: voxel positions and colors
 
     # offline trained GMMs and lookup table
     global gaussian_mixture_models
     global lookup_table
 
-    # 3D reconstrucion for the frame
+    # 3D reconstruction for the frame
     visible_voxels, labels, centers = reconstruct_voxels(n_frame)
 
     # 2D coordinates of the projected voxels for each camera
@@ -179,8 +181,8 @@ def set_voxel_positions(width, height, depth, n_frame):
 
         # storing all 2D pixel coordinates for each person for this camera
         person_pixels = {0: [], 1: [], 2: [], 3: []}
-        for pixel, label in image_points:
-            person_pixels[label].append(pixel)
+        for pixel, label_of_pixel in image_points:
+            person_pixels[label_of_pixel].append(pixel)
         people_pixels.append(person_pixels)
 
         # get number of connected component for this camera and store it
@@ -208,9 +210,9 @@ def set_voxel_positions(width, height, depth, n_frame):
                                                          for x in pixels]) // 1.5
 
         # build the mask
-        for x, y in pixels:
-            if y < waist:
-                mask[y, x] = 255
+        for x_pixel, y_pixel in pixels:
+            if y_pixel < waist:
+                mask[y_pixel, x_pixel] = 255
 
         log_likelihoods = []  # log likelihoods of the 4 GMMs for this person
 
@@ -220,7 +222,7 @@ def set_voxel_positions(width, height, depth, n_frame):
 
             # for each pixel of the person in HSV
             for pixel in cv.cvtColor(frames[n_camera_with_best_separation][n_frame], cv.COLOR_BGR2HSV)[
-                    mask == 255].tolist():
+                mask == 255].tolist():
                 # get the likelihood score for this model and accumulate it for all pixels
                 log_likelihood += gaussian_mixture_models[i_gmm].predict2(np.array(pixel, dtype=np.float32))[0][
                     0]
@@ -243,7 +245,7 @@ def set_voxel_positions(width, height, depth, n_frame):
 
             # for each person
             for person, _ in best_person:
-                # count occurences
+                # count occurrences
                 if person in unique_best_person:
                     count[person] += 1
 
@@ -251,16 +253,16 @@ def set_voxel_positions(width, height, depth, n_frame):
             for person in range(4):
                 if count[person] > 1:  # if it is duplicate
                     probs_of_person = []
-                    # extractin likelihood scores of the duplicates
-                    for x in best_person:
-                        if x[0] == person:  # the duplicate
-                            probs_of_person.append(x[1])
+                    # extracting likelihood scores of the duplicates
+                    for x_pixel in best_person:
+                        if x_pixel[0] == person:  # the duplicate
+                            probs_of_person.append(x_pixel[1])
                         else:  # append value large enough to not be chosen for other people
                             probs_of_person.append(1)
                     # get the duplicate guess with the lowest score
                     index_of_missing_person = np.argmin(probs_of_person)
                     best_person[index_of_missing_person] = (missing_person, min(
-                        probs_of_person))  # substitue it with the missing person
+                        probs_of_person))  # substitute it with the missing person
 
     # tracking section
     centers_right = [(0, 0), (0, 0), (0, 0), (0, 0)]
@@ -268,9 +270,9 @@ def set_voxel_positions(width, height, depth, n_frame):
     # relabel centers to match the offline labels
     for i_label in range(4):
         new_label = best_person[i_label][0]
-        x = centers[i_label][0]
-        y = centers[i_label][1]
-        centers_right[new_label] = (x, y)
+        x_pixel = centers[i_label][0]
+        y_pixel = centers[i_label][1]
+        centers_right[new_label] = (x_pixel, y_pixel)
 
     centers_list.append(centers_right)
 
@@ -287,13 +289,13 @@ def set_voxel_positions(width, height, depth, n_frame):
 def get_cam_positions():
     # Generates dummy camera locations at the 4 corners of the room
     positions = []
-    for camera_i in range(1, 5):
-        s = cv.FileStorage(
-            f"data/cam{camera_i}/config.xml", cv.FileStorage_READ)
-        tvec_extr = s.getNode('tvec_extr').mat()
-        R = s.getNode('R_MAT').mat()
+    for i_camera in range(1, 5):
+        fs = cv.FileStorage(
+            f"data/cam{i_camera}/config.xml", cv.FileStorage_READ)
+        tvec_extr = fs.getNode('tvec_extr').mat()
+        R = fs.getNode('R_MAT').mat()
         positions.append(np.dot(-R.T, tvec_extr))
-        s.release()
+        fs.release()
     positions = np.stack(positions)
     # positions = normalize(positions.squeeze(), axis=0)
     positions = positions.squeeze()
@@ -311,17 +313,17 @@ def get_cam_positions():
 def get_cam_rotation_matrices():
     # Generates dummy camera rotation matrices, looking down 45 degrees towards the center of the room
     cam_rotations = []
-    for camera_i in range(1, 5):
-        s = cv.FileStorage(
-            f"data/cam{camera_i}/config.xml", cv.FileStorage_READ)
-        tvec_extr = s.getNode('tvec_extr').mat()
-        R = s.getNode('R_MAT').mat()
+    for i_camera in range(1, 5):
+        fs = cv.FileStorage(
+            f"data/cam{i_camera}/config.xml", cv.FileStorage_READ)
+        tvec_extr = fs.getNode('tvec_extr').mat()
+        R = fs.getNode('R_MAT').mat()
 
         t1 = np.hstack((R, tvec_extr))
         t1 = np.vstack((t1, [0, 0, 0, 1]))
         # swap y and z
         t1[:, [1, 2]] = t1[:, [2, 1]]
-        # invert x rotation of the camears
+        # invert x rotation of the cameras
         t1[:, 1] = -t1[:, 1]
         # transform to mat4
         rot = glm.mat4(t1.T)
@@ -367,9 +369,9 @@ def reconstruct_all_voxels():
         _, labels, centers = cv.kmeans(
             voxels_to_cluster, 4, None, criteria, 20, cv.KMEANS_RANDOM_CENTERS)
         voxels_to_remove = []
-        for label, center in enumerate(centers):
+        for label_center, center in enumerate(centers):
             filtered_list = [lst for lst, current_lab in zip(
-                visible_voxels, labels) if current_lab == label]
+                visible_voxels, labels) if current_lab == label_center]
             avg_dist_from_center = 0
             for x, z, y in filtered_list:
                 avg_dist_from_center += distance.euclidean((x, y), center)
@@ -393,7 +395,7 @@ def reconstruct_all_voxels():
 
 
 def create_cube(width, height, depth):
-    "creates a solid with resolution 100x100x100 with the current inputs"
+    """creates a solid with resolution 100x100x100 with the current inputs"""
     cube = []
     for x in np.arange(-width, width, 80):
         for y in np.arange(-depth // 2, depth // 2, 80):
@@ -412,12 +414,12 @@ def get_gaussian_mixture_models():
                         {0: [], 1: [], 2: [], 3: []}]
 
     # initialize the 4 GMMs
-    gaussian_mixture_models = {0: cv.ml.EM_create(
+    gmm_s = {0: cv.ml.EM_create(
     ), 1: cv.ml.EM_create(), 2: cv.ml.EM_create(), 3: cv.ml.EM_create()}
 
     # set the cluster number of each GMM to 3
     for person_i in range(4):
-        gaussian_mixture_models[person_i].setClustersNumber(3)
+        gmm_s[person_i].setClustersNumber(3)
 
     # get the reconstructions of the 4 frames
     all_visible_voxels, all_labels = reconstruct_all_voxels()
@@ -440,17 +442,17 @@ def get_gaussian_mixture_models():
                 camera_matrices[i_camera], distance_coefficients[i_camera])
             x_2d, y_2d = (int(coordinates_2d.ravel()[0]), int(
                 coordinates_2d.ravel()[1]))
-            # store the infos needed:
+            # store the information needed:
             # tuple (2d pixel, clustering label, original color)
             image_points.append(
                 ((x_2d, y_2d), all_labels[i_camera][i_label][0], frames[i_camera][chosen_frame][y_2d, x_2d]))
-        pixels_colors.append(image_points)  # store infos of each camera
+        pixels_colors.append(image_points)  # store information of each camera
 
     # getting the coordinates of each person for each camera
     for i_camera, pixels_color in enumerate(pixels_colors):
         # for each person
-        for pixel, label, color in pixels_color:
-            person_to_colors[i_camera][label].append(pixel)
+        for pixel, label_pixel, color in pixels_color:
+            person_to_colors[i_camera][label_pixel].append(pixel)
 
     # hand built map to match the offline labels of the 4 cameras
     best_person = [[0, 1, 2, 3], [2, 1, 3, 0], [2, 0, 3, 1], [2, 3, 1, 0]]
@@ -468,7 +470,7 @@ def get_gaussian_mixture_models():
             mask = np.zeros(
                 frames[i_camera][cam_to_frame[i_camera]].shape[:2], np.uint8)
             waist = np.max([x[1] for x in pixels]) - \
-                np.min([x[1] for x in pixels]) // 1.5
+                    np.min([x[1] for x in pixels]) // 1.5
             for x, y in pixels:
                 if y < waist:
                     mask[y, x] = 255
@@ -477,7 +479,7 @@ def get_gaussian_mixture_models():
 
             # store the colors in the corresponding person for each view using the map
             person_training_data[best_person[i_camera]
-                                 [person]].append(person_pixels)
+            [person]].append(person_pixels)
 
     # training the 4 GMMs
     for i_person in range(4):
@@ -485,46 +487,48 @@ def get_gaussian_mixture_models():
         data = [item for sublist in person_training_data[i_person]
                 for item in sublist]
         # train the gaussian mixture model
-        gaussian_mixture_models[i_person].trainEM(
+        gmm_s[i_person].trainEM(
             np.array(data, dtype=np.float32))
 
-    return gaussian_mixture_models
+    return gmm_s
 
 
 def create_lookup_table(width, height, depth):
-    "Returns the lookup table mapping each voxels to its correspongin 3D and 2D coordinates for each camera"
-    "Inputs are for the placement of the solid for the reconstruction"
+    """
+    Returns the lookup table mapping each voxel to its corresponding 3D and 2D coordinates for each camera
+    Inputs are for the placement of the solid for the reconstruction
+    """
     # create solid
-    voxel_positions = np.array(create_cube(
+    voxel_positions_list = np.array(create_cube(
         width, height, depth), dtype=np.float32)
     # initialize look up table
-    lookup_table = np.zeros((voxel_positions.shape[0], 5, 4))
-    for camera_i in range(1, 5):  # for each camera
+    lut = np.zeros((voxel_positions_list.shape[0], 5, 4))
+    for i_camera in range(1, 5):  # for each camera
         # load parameters
-        s = cv.FileStorage(
-            f"data/cam{camera_i}/config.xml", cv.FileStorage_READ)
-        camera_matrix = s.getNode('camera_matrix').mat()
-        dist_coeffs = s.getNode('dist_coeffs').mat()
-        tvec_extr = s.getNode('tvec_extr').mat()
-        rvec_extr = s.getNode('rvec_extr').mat()
-        s.release()
+        fs = cv.FileStorage(
+            f"data/cam{i_camera}/config.xml", cv.FileStorage_READ)
+        camera_matrix = fs.getNode('camera_matrix').mat()
+        dist_coeffs = fs.getNode('dist_coeffs').mat()
+        tvec_extr = fs.getNode('tvec_extr').mat()
+        rvec_extr = fs.getNode('rvec_extr').mat()
+        fs.release()
         # for each 3D point of the voxel solid
-        for i, pos in enumerate(voxel_positions):
+        for i_voxel, pos in enumerate(voxel_positions_list):
             # project the point in the 2D image plane for this camera
-            imgpoint, _ = cv.projectPoints(
+            image_point, _ = cv.projectPoints(
                 pos, rvec_extr, tvec_extr, camera_matrix, dist_coeffs)
-            imgpoint = imgpoint.ravel()
-            x = imgpoint[0]
-            y = imgpoint[1]
-            if x >= 0 and x < 644 and y >= 0 and y < 486:  # if the 2D pixel is in range of the frame
+            image_point = image_point.ravel()
+            x = image_point[0]
+            y = image_point[1]
+            if 0 <= x < 644 and 0 <= y < 486:  # if the 2D pixel is in range of the frame
                 # store 2D and 3D coordinates
-                lookup_table[i, :, camera_i -
-                             1] = [int(pos[0]), int(pos[1]), int(pos[2]), int(x), int(y)]
+                lut[i_voxel, :, i_camera - 1] = [int(pos[0]), int(pos[1]), int(pos[2]), int(x), int(y)]
 
-    np.savez('data/lookup_table', lookup_table=lookup_table)
-    return voxel_positions, lookup_table
+    np.savez('data/lookup_table', lookup_table=lut)
+    return voxel_positions_list, lut
 
-## EXECUTION ##
+
+# EXECUTION
 
 # creating the lookup table for reconstruction at the start of execution
 start_lookup = time()
@@ -556,7 +560,7 @@ for camera_i in range(4):
 gaussian_mixture_models = get_gaussian_mixture_models()
 
 # next section is for the tracking task, which is done without visualization
-# run this file rather than the executable file with the tracking flag on to get the trackings
+# run this file rather than the executable file with the tracking flag on to get the tracking
 if tracking:
     # perform the online for all frames
     for f in range(0, 270):
@@ -586,13 +590,13 @@ if tracking:
                 dist = np.linalg.norm(np.array(c) - np.array(old_c[label]))
                 if dist < 10:
                     cv.line(paths2, (int(x), int(y)), (int(old_x),
-                            int(old_y)), labels_to_color[label], 2)
+                                                       int(old_y)), labels_to_color[label], 2)
                 if dist < 20:
                     cv.line(paths3, (int(x), int(y)), (int(old_x),
-                            int(old_y)), labels_to_color[label], 2)
+                                                       int(old_y)), labels_to_color[label], 2)
                 if dist < 8:
                     cv.line(paths4, (int(x), int(y)), (int(old_x),
-                            int(old_y)), labels_to_color[label], 2)
+                                                       int(old_y)), labels_to_color[label], 2)
 
     cv.imwrite("paths_v2.png", paths)
     cv.imwrite("paths_v3_10.png", paths2)
